e1ffccb3f46abf9fbed5cc9e04d8a544f7114403
oleska-sky <130528774+oleska-sky@users.noreply.github.com>
2023-07-01 04:50:52 +0500

    Add files via upload
    реализация токенов

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+ID приложения 51692363
+https://oauth.vk.com/authorize?client_id=51692363&scope=photos&response_type=token&v=5.131&display=page&state=123456&revoke=1
+
+
+
+"""
+
+comunity_token = ''
+access_token = ''
+
+db_url_object = 'postgresql://postgres:password@localhost:5432/db_diplom'
+
